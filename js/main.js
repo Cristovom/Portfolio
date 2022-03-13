139bec37fc57554d3333c681435915cdda23a925
Cristovom Nunes <cristovom007@gmail.com>
2022-03-13 18:02:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // token arthur === IGQVJXdjJxOHJ6MGl6eFZARRWZAuWGJVX3hXeS0zN3FyNkdWQURRNFFRNFRXMnN5dXk2N3h6Sy1faWhISW1ITWw4U0FXcVFXV2dIQ0NMT1NSYkR0QkVEZAm9zbk5CY1RyV3BNYi1ESDZAnbDRuYzJzTUFoUAZDZD
 $.ajax({
     type:"GET",
-    url: "https://graph.instagram.com/me/media?fields=media_type,media_url,id,caption,permalink&limit=10&access_token=IGQVJYU2o1cWVwdnZAKdEJESjRSQjJ3OWV3Y2VOaGFYdlJTeFN1UC1SXzdERmM5ZAFZAyZAkwzWmNtcHAtTldST2JUYmpzNFZA1NWR3NFFidDVOYUhLb0ZABTU9JaWlNTVZAoX08ySGxPV1l5MXZAONVpfRVJQVAZDZD",
+    url: "https://graph.instagram.com/me/media?fields=media_type,media_url,id,caption,permalink&limit=10&access_token=IGQVJWM25wRnktaXFIM3dEeUJUZAlhWNDdINjVyZAUJJSzU5ZAjg3ZADJjc0NieEEzMDFfcHdMRzROS0FvRE5oakpPRHI5V0ZA0eEtpLUJ2cGpHZAXFxRDBoRWw1b1hLVzR4RkhPXzA1RktmUVRxRHhPOHRkNwZDZD",
     success: function(data){
     console.log(data);
     for( x in data.data ){
